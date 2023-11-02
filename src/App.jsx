ce95371248d7c0b69ffe6e7c521d00eef06986b2
Jose Miguel Valiente <josemiguel.valiente@star-robotics.com>
2023-11-02 10:38:18 +0100

    Add mocks to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,59 @@
 import Map, { Marker, Popup } from "react-map-gl/maplibre";
 import { useState } from "react";
+import Panel from "./components/Panel/Panel";
+import Card from "./components/Card/Card";
 
-const Card = () => {
-  return (
-    <>
-      <div class="block b-0 p-0 rounded-lg bg-white dark:bg-neutral-700">
-        <div class="relative overflow-hidden bg-cover bg-no-repeat">
-          <img
-            class="rounded-t-lg"
-            src="https://tecdn.b-cdn.net/img/new/slides/041.webp"
-            alt=""
-          />
-        </div>
-        <div class="p-6">
-          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-            Card title
-          </h5>
-          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="text-base text-neutral-600 dark:text-neutral-200">
-            <small class="text-neutral-500 dark:text-neutral-400">
-              Last updated 3 mins ago
-            </small>
-          </p>
-        </div>
-      </div>
-    </>
-  );
-};
+const markers = [
+  { longitude: -3.7905054, latitude: 40.2867324, type: "Dog" },
+  { longitude: -3.6905054, latitude: 40.4867324, type: "Car" },
+];
 
 function App() {
-  const [showPopup, setShowPopup] = useState(true);
-
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupCoordinates, setPopupCoordinates] = useState([0, 0]);
   const MAPSTYLE = `https://api.maptiler.com/maps/streets/style.json?key=${
     import.meta.env.VITE_MAPTILER_KEY
   }`;
 
   return (
-    <Map
-      initialViewState={{
-        latitude: 40.2867324,
-        longitude: -3.7905054,
-        zoom: 15,
-        bearing: 80,
-        pitch: 80,
-      }}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle={MAPSTYLE}
-    >
-      <Marker
-        longitude={-3.7905054}
-        latitude={40.2867324}
-        anchor="bottom"
-        onClick={(e) => {
-          e.originalEvent.stopPropagation();
-          setShowPopup(true);
+    <>
+      <Map
+        initialViewState={{
+          latitude: 40.2867324,
+          longitude: -3.7905054,
+          zoom: 12,
+          bearing: 0,
+          pitch: 60,
         }}
-      />
-      {showPopup && (
-        <Popup
-          longitude={-3.7905054}
-          latitude={40.2867324}
-          anchor="bottom"
-          offset={60}
-          padding={0}
-          onClose={() => setShowPopup(false)}
-        >
-          <Card />
-        </Popup>
-      )}
-    </Map>
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle={MAPSTYLE}
+      >
+        {markers.map((marker) => (
+          <Marker
+            longitude={marker.longitude}
+            latitude={marker.latitude}
+            anchor="bottom"
+            onClick={(e) => {
+              e.originalEvent.stopPropagation();
+              setPopupCoordinates([marker.longitude, marker.latitude]);
+              setShowPopup(true);
+            }}
+          />
+        ))}
+        <Panel />
+        {showPopup && (
+          <Popup
+            longitude={popupCoordinates[0]}
+            latitude={popupCoordinates[1]}
+            anchor="bottom"
+            offset={60}
+            onClose={() => setShowPopup(false)}
+          >
+            <Card />
+          </Popup>
+        )}
+      </Map>
+    </>
   );
 }
 
